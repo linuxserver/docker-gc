3e7b48c58ecf7a6f935f774ca9f1aec56f8a7b63
sparklyballs <sparklyballs@gmail.com>
2016-11-20 13:40:01 +0000

    update execution scripts

--- run-arm.sh
+++ run-arm.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+set -ex
+
 docker pull lsiodev/docker-gc-armhf
 
 docker run --rm \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v "${WORKSPACE}"/etc:/etc lsiodev/docker-gc FORCE_OPTIONS_IMAGE FORCE_OPTIONS_CONTAINER || true
+	-v "${WORKSPACE}"/etc:/etc lsiodev/docker-gc-armhf FORCE_OPTIONS_IMAGE FORCE_OPTIONS_CONTAINER || true
 
--- run-x86-64.sh
+++ run-x86-64.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -ex
+
 docker pull lsiodev/docker-gc
 
 docker run --rm \
