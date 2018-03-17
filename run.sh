bcd178c353707a2ba927d4e81542543cf5413bbc
sparklyballs <sparklyballs@gmail.com>
2018-03-17 08:41:22 +0000

    improve run script

--- run.sh
+++ run.sh
@@ -3,26 +3,28 @@
 set -ex
 
 # clean any prexisting variable values
-unset BASEIMAGE_ARCH CLEANUP_ARCH SHELLCHECK_ARCH
+unset BASEIMAGE_ARCH CLEANUP_ARCH README_SYNC SHELLCHECK_ARCH
 
-# set arch for baseimage
+# test if node is for readme-sync
 if [[ "${NODE_LABELS}"  == *"README"* ]]; then
-BASEIMAGE_ARCH="readme"
-elif [[ "${NODE_LABELS}"  == *"ARM64"* ]]; then
-BASEIMAGE_ARCH="arm64"
-elif [[ "${NODE_LABELS}"  == *"ARMHF"* ]]; then
-BASEIMAGE_ARCH="armhf"
-elif [[ "${NODE_LABELS}"  == *"X86"* ]]; then
-BASEIMAGE_ARCH="x86-64"
+	README_SYNC="true"
 fi
 
-# set arch for cleanup and shellcheck
-if [[ "${NODE_LABELS}" == *"ARM"* ]]; then
-CLEANUP_ARCH="-${BASEIMAGE_ARCH}"
-SHELLCHECK_ARCH="-armhf"
-else
-CLEANUP_ARCH=""
-SHELLCHECK_ARCH=""
+# set arch for shellcheck readme-sync and cleanup based on node lables
+if [[ "${NODE_LABELS}"  == *"X86"* ]]; then
+	BASEIMAGE_ARCH="x86-64"
+	README_SYNC_ARCH=""
+	SHELLCHECK_ARCH=""
+	CLEANUP_ARCH=""
+elif [[ "${NODE_LABELS}" == *"ARM"* ]]; then
+	SHELLCHECK_ARCH="-armhf"
+	README_SYNC_ARCH="-armhf"
+		if [[ "${NODE_LABELS}"  == *"ARM64"* ]]; then
+		BASEIMAGE_ARCH="arm64"
+		elif [[ "${NODE_LABELS}"  == *"ARMHF"* ]]; then
+		BASEIMAGE_ARCH="armhf"
+		fi
+	CLEANUP_ARCH="-${BASEIMAGE_ARCH}"
 fi
 
 # pull docker images reading from docker-gc excludes file, ignoring readme-sync and shellcheck
@@ -45,8 +47,8 @@ fi
 done < "${WORKSPACE}"/exclude_list
 
 # pull shellcheck image
-if [[ "$BASEIMAGE_ARCH" == "readme" ]]; then \
-docker pull lsiodev/readme-sync-armhf
+if [[ "${README_SYNC}" == "true" ]]; then \
+docker pull lsiodev/readme-sync"${README_SYNC_ARCH}"
 else
 docker pull lsiodev/shellcheck"${SHELLCHECK_ARCH}"
 fi
